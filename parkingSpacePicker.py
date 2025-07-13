b6e014ae98b8fa04a987de8c01a2a173c5dc7391
Bertrand <mutanganabertrand@gmail.com>
2025-07-13 19:24:33 +0200

    Add pickle object to store positions

--- parkingSpacePicker.py
+++ parkingSpacePicker.py
@@ -3,7 +3,33 @@ import pickle
 
 img = cv2.imread("carParkImg.png")
 
+width, height = 107, 48
+
+try:
+    with open('CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+def mouseClick(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            if pos[0] < x < pos[0] + width and pos[1] < y < pos[1] + height:
+                posList.pop(i)
+                break
+    with open('CarParkPos', 'wb') as f:
+        pickle.dump(posList, f)
+
+
 while True:
-    cv2.rectangle(img, (50, 192), (157, 240), (255, 0, 255), 2)
+    img = cv2.imread("carParkImg.png")
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
     cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouseClick)
     cv2.waitKey(1)
